b724658f572ee126746e6a393428e3f8a37c7056
manyi <zhaomanyi2005@gmail.com>
2023-07-10 01:00:22 -0700

    updated level

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Gyro.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+// todo write a gyro class that can display the robot's orientation on the driver hub
+public class Gyro {
+
+    public Gyro(){}
+
+    // this function will return the robot heading
+    public double getHeading(){}
+}
