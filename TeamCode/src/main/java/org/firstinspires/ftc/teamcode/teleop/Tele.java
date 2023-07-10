76adcf3014d54dd9293a2d289b2639e08a14a658
manyi <zhaomanyi2005@gmail.com>
2023-07-10 01:04:30 -0700

    updated level

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Tele.java
@@ -2,13 +2,14 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.robot.Robot;
 
 import org.firstinspires.ftc.teamcode.robot.Drivebase;
 
 @TeleOp
 public class Tele extends LinearOpMode {
 
-    Drivebase robot;
+    Robot robot;
 
     @Override
     public void runOpMode() throws InterruptedException {
